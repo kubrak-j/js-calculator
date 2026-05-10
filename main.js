570f00220ce45af7aa853331c5bcb478f0b613a8
Jaroslav Kubrak <jarikkubrak@gmail.com>
2026-05-10 19:38:28 +0200

    refactor: clean up HTML/CSS and implement operator selection logic
    - Consolidated button styles into a single .btnStyle class in CSS.
    - Renamed classes for better readability and maintainability.
    - Implemented operator selection using a NodeList of operator buttons.
    - Stored chosen mathematical operators in a variable for future calculations.
    - Cleaned up JavaScript code by removing unused function stubs.

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const numButtons = document.querySelectorAll(`.numBtn`);
-const displayInput = document.getElementById('displayInput');
-
-console.log(numButtons);
+const displayInput = document.getElementById(`displayInput`);
+const acBtn = document.getElementById(`acBtn`);
+const equalsBtn = document.getElementById(`equalsBtn`);
+const operatorBtns = document.querySelectorAll(`.operatorBtn`);
+let operator = null;
 
 numButtons.forEach((item) => {item.onclick = () => {displayInput.value += item.textContent}});
+
+operatorBtns.forEach((item) => {item.onclick = () => {operator = item.textContent}});
+
+acBtn.onclick = () => {displayInput.value = `0`};
