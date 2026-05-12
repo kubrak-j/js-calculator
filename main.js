2ae45868ec89113ed253be23beded3ea8559000e
Jaroslav Kubrak <jarikkubrak@gmail.com>
2026-05-12 17:10:29 +0200

    feat: implement core calculator logic for arithmetic operations
    - Created variables to store first number, second number, and operator.
    - Implemented calculate function using if/else logic.
    - Fixed the leading zero bug when entering the first digit.
    - Added AC button functionality to reset the display to "0".
    - Integrated Number() conversion for accurate mathematical calculations.

--- main.js
+++ main.js
@@ -3,10 +3,68 @@ const displayInput = document.getElementById(`displayInput`);
 const acBtn = document.getElementById(`acBtn`);
 const equalsBtn = document.getElementById(`equalsBtn`);
 const operatorBtns = document.querySelectorAll(`.operatorBtn`);
+
 let operator = null;
 
-numButtons.forEach((item) => {item.onclick = () => {displayInput.value += item.textContent}});
+let previousNum = null;
+let currentNum = null;
 
-operatorBtns.forEach((item) => {item.onclick = () => {operator = item.textContent}});
+let result = null;
 
-acBtn.onclick = () => {displayInput.value = `0`};
+numButtons.forEach((item) => {item.onclick = () => {
+
+    if(displayInput.value === 0 || displayInput.value === `0`){
+        displayInput.value = ``;
+    }
+    displayInput.value += item.textContent;
+}});
+
+operatorBtns.forEach((item) => {item.onclick = () => {
+    operator = item.textContent;
+    let firstInputNum = displayInput.value;
+    previousNum = Number(firstInputNum);
+    displayInput.value = ``;
+}});
+
+equalsBtn.onclick = calculate;
+
+function calculate() {
+
+    try {
+
+        let secondInputNum = displayInput.value;
+        currentNum = Number(secondInputNum);
+
+        if(operator === `+`){
+            result = previousNum + currentNum;
+            displayInput.value = result;
+        }
+        else if(operator === `-`){
+            result = previousNum - currentNum;
+            displayInput.value = result;
+        }
+        else if(operator === `*`){
+            result = previousNum * currentNum;
+            displayInput.value = result;
+        }
+        else if(operator === `/`){
+            if(currentNum === 0 || currentNum === `0`){
+                displayInput.value = `Error: You can't divide by zero`;
+            }
+            else {
+                result = previousNum / currentNum;
+                displayInput.value = result;
+            }
+        }
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+
+acBtn.onclick = () => {
+    previousNum = null;
+    currentNum = null;
+    result = null;
+    displayInput.value = `0`;
+};
